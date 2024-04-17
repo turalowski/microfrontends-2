b044f806f092af4caed58eac66d9a7b841072073
Tural Hajiyev <thajiyev9@outlook.com>
2024-04-18 00:35:48 +0200

    integrated marketing app to container

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,12 @@
+import { mount as mountMarketingApp } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react';
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mountMarketingApp(ref.current);
+  });
+
+  return <div ref={ref} />;
+};
