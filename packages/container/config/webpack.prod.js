cc9aa8b9c1bf67257daee904f91eee4078979bf4
Tural Hajiyev <thajiyev9@outlook.com>
2024-04-20 22:06:14 +0200

    removed file name

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,7 +8,6 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js',
     publicPath: '/container/latest/',
   },
   plugins: [
